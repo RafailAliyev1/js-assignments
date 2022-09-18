9e8d341248757b6e232487033feda139de81435c
RafailAliyev1 <96453319+RafailAliyev1@users.noreply.github.com>
2022-09-19 02:00:29 +0300

    Update 08-objects-tasks.js

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,7 +23,9 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    this.width = width;
+    this.height = height;
+    this.getArea = () => this.width * this.height;
 }
 
 
@@ -38,7 +40,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj);
 }
 
 
@@ -54,6 +56,10 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
+//    const obj = JSON.parse(json);
+//    const values = Object.values(obj);
+    
+//    return new proto.constructor(...values);
     throw new Error('Not implemented');
 }
 
